4b314ed20f15d19a53f93c20622ff5dcfe54ba5a
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-02-03 20:35:52 -0600

    two call backs

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-
-
-
 const strlength = (inputName, cb) => {
     cb(inputName.length)
 }
 
 const cb = nameLength => {
-    console.log(nameLength)
+    console.log(`you name is ${nameLength} characters long!`)
 }
 
 strlength("Veekshithhh", cb)
+
+const willThanosKillMe = (ipname, cb1, cb2) => {
+    if(ipname.length%2 === 0){
+        cb1();
+    } else {
+        cb2();
+    }
+}
+
+const thanosAlive = () => {
+    console.log("Yay i love sadhvika");
+}
+
+const thanosDead = () => {
+    console.log("Yay i anyway love sadhvika");
+}
+
+willThanosKillMe("Sadhvikaa", thanosAlive, thanosDead)
